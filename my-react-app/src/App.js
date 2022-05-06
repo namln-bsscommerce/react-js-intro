6277a2bca3929f71190e14fd49ece6adf44eec0c
namln-bsscommerce <namln@bsscommerce.com>
2022-05-06 17:59:07 +0700

    06-05 - timer and navigation tabs

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,39 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const tabs = ['users', 'companies', 'countries'];
+
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [items, setItems] = useState([]);
+    const [activeTab, setActiveTab] = useState('users');
+
+    useEffect(() => {
+        fetch(`https://6274bb9a345e1821b22ecc93.mockapi.io/api/v1/users`)
+            .then(res => res.json())
+            .then(res => setItems(res));
+    }, [])
+
     return (
-        <div className="App">
-            Hello World
+        <div className='App'>
+            <h1 className='title'>
+                News reading app
+            </h1>
+            <div className='timer'>
+                Total reading time:&nbsp;<b>{counter}</b>&nbsp;s
+            </div>
+            <div className="tabs">
+                {tabs.map(tab => (
+                    <li className='tab' key={tab}>
+                        <button>{tab.charAt(0).toUpperCase() + tab.slice(1)}</button>
+                    </li>
+                ))}
+            </div>
+            <ul className='content'>
+                {items.map(item => (
+                    <li key={item.id}>{item.id}</li>
+                ))}
+            </ul>
         </div>
     );
 }
